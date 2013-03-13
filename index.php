5fa3e7421a5ddf34ea059c9021e91bbfce255744
nandy-andy <nandy@wikia-inc.com>
2013-03-13 23:41:52 +0100

    added new route for single post view in index.php

--- index.php
+++ index.php
@@ -10,4 +10,8 @@ $app->get('/', function() use ($app) {
     return $app['twig']->render('blog.html');
 });
 
+$app->get('/post/{id}', function($id) use ($app) {
+	return $app['twig']->render('single.html');
+});
+
 $app->run();
