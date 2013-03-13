f6fa2a1cbb098c0ed6ed5e067d8d7d91f2be49f4
nandy-andy <nandy@wikia-inc.com>
2013-03-13 22:50:58 +0100

    registered twig provider, configured its template main direcotry and created new routing rule instead of old, example rule; so now entering blog.lc page results with blog page with listed posts

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 require_once __DIR__ . '/vendors/silex/autoload.php';
 
 $app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+	'twig.path' => __DIR__. '/templates',
+));
 
-$app->get('/hello', function() {
-    return 'Hello!';
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('blog.html');
 });
 
 $app->run();
