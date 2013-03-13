8a0d2d161a02fe74c209f19b77a756ae7f0d6737
nandy-andy <nandy@wikia-inc.com>
2013-03-13 23:11:18 +0100

    created something-hard-to-call-autoloader for assetic and allinone.php file to server combined JS files

---
+++ vendors/assetic/autoload.php
@@ -0,0 +1,30 @@
+<?php
+include_once(__DIR__ . '/Assetic/Util/PathUtils.php');
+include_once(__DIR__ . '/Assetic/Util/TraversableString.php');
+
+include_once(__DIR__ . '/Assetic/Asset/AssetInterface.php');
+
+include_once(__DIR__ . '/Assetic/AssetManager.php');
+include_once(__DIR__ . '/Assetic/Filter/FilterInterface.php');
+include_once(__DIR__ . '/Assetic/FilterManager.php');
+
+include_once(__DIR__ . '/Assetic/Asset/AssetCollectionInterface.php');
+
+include_once(__DIR__ . '/Assetic/Asset/Iterator/AssetCollectionFilterIterator.php');
+include_once(__DIR__ . '/Assetic/Asset/Iterator/AssetCollectionIterator.php');
+
+include_once(__DIR__ . '/Assetic/Filter/FilterCollection.php');
+
+include_once(__DIR__ . '/Assetic/Asset/AssetCache.php');
+include_once(__DIR__ . '/Assetic/Asset/AssetCollection.php');
+include_once(__DIR__ . '/Assetic/Asset/AssetReference.php');
+include_once(__DIR__ . '/Assetic/Asset/BaseAsset.php');
+include_once(__DIR__ . '/Assetic/Asset/FileAsset.php');
+include_once(__DIR__ . '/Assetic/Asset/GlobAsset.php');
+include_once(__DIR__ . '/Assetic/Asset/HttpAsset.php');
+include_once(__DIR__ . '/Assetic/Asset/StringAsset.php');
+
+include_once(__DIR__ . '/Assetic/Factory/AssetFactory.php');
+include_once(__DIR__ . '/Assetic/Factory/Worker/WorkerInterface.php');
+
+include_once(__DIR__ . '/assetic/functions.php');
